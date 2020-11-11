68c1894b9d0f75eb0b6d42acffc12f7c74539e89
JantsoP <jantso.porali@gmail.com>
2020-11-11 16:47:18 +0200

    add the script

---
+++ nenapaiva-summa-script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Setting global variables
+# Ask the user for their name
+echo "What Nenäpäivä page would you like to download? (Provide full URL)"
+read nenapage
+echo What would be the name of the saved webpage?
+read pagename
+
+echo Creating needed dirs.
+mkdir nenapaiva-files
+clear
+echo All done. Starting script in 5 seconds.
+sleep 5
+
+while :
+do
+    clear
+    echo Wakey wakey, eggs and bakey
+    echo
+    echo
+	echo Get Nenapaiva page
+    echo
+    echo
+    wget $nenapage -O nenapaiva-files/$pagename
+    echo Print webpage and then grab total sum of current donations
+    echo
+    echo
+    echo Print result and then remove extra div after euro sign
+    cat nenapaiva-files/$pagename | grep "€ </div>" > nenapaiva-files/stripped-sum.txt
+    cat nenapaiva-files/stripped-sum.txt | cut -d "<" -f1 > nenapaiva-files/stripped-space.txt
+    echo
+    echo
+    echo Remove extra space from end of line
+    sed 's/[[:blank:]]*$//' nenapaiva-files/stripped-space.txt > donations.txt
+    echo
+    echo
+    echo Print the result to console as well
+    cat donations.txt
+    echo
+    echo
+    echo I sleep now for 30 seconds. To stop this script, just press CTRL+C
+	sleep 30
+done
