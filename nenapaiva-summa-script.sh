6776319a6db66dc267ad11b42792716d580a7a2c
JantsoP <jantso.porali@gmail.com>
2020-11-11 17:57:32 +0200

    updated code
    added missing comments

--- nenapaiva-summa-script.sh
+++ nenapaiva-summa-script.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Setting global variables
-# Ask the user for their name
+# Ask the user for their Nenäpäivä URL
 echo "What Nenäpäivä page would you like to download? (Provide full URL)"
 read nenapage
+# Ask which filename they would like to save it
 echo What would be the name of the saved webpage?
 read pagename
 
+# Create nenapaiva-files dir
 echo Creating needed dirs.
 mkdir nenapaiva-files
 clear
 echo All done. Starting script in 5 seconds.
 sleep 5
 
+# Lets loop this thing.
 while :
 do
     clear
